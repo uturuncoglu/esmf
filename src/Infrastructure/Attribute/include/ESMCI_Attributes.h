e15551781dd8f7c644aa1ff17ed018ca0f500210
Ben Koziol <ben.koziol@noaa.gov>
2018-10-24 11:31:54 -0600

    WIP: Minor

--- src/Infrastructure/Attribute/include/ESMCI_Attributes.h
+++ src/Infrastructure/Attribute/include/ESMCI_Attributes.h
@@ -53,9 +53,9 @@ class esmf_attrs_error : public std::exception
 {
 public:
   esmf_attrs_error(const string &code_name, int rc, const string &msg) {
-    string theMessage = "Error/Return Code " + std::to_string(rc) + " (" + \
-                        code_name + "): " + msg;
-    this->msg = theMessage;
+    string the_msg = "Error/Return Code " + std::to_string(rc) + " (" + \
+                     code_name + "): " + msg;
+    this->msg = the_msg;
   }
 
   const char* what() const noexcept {
