254cfac10c183ebf8ecccf92cfb3966b69dd4e1f
Ben Koziol <ben.koziol@noaa.gov>
2019-01-21 08:42:40 -0700

    WIP: Working attribute get/set test on array

---
+++ src/Infrastructure/Array/tests/ESMF_ArrayAttributesUTest.F90
@@ -0,0 +1,107 @@
+! $Id$
+!
+! Earth System Modeling Framework
+! Copyright 2002-2018, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
+
+#define FILENAME "src/Infrastructure/Attribute/test/ESMF_ArrayAttributesUTest.F90"
+
+#include "ESMF_Macros.inc"
+#include "ESMF.h"
+
+!==============================================================================
+!==============================================================================
+!==============================================================================
+
+program ESMF_ArrayAttributesUTest
+
+  !============================================================================
+  !BOP
+  ! !PROGRAM: ESMF_AttributesUTest - Test general Attributes usage
+  !
+  ! !DESCRIPTION:
+  !
+  !----------------------------------------------------------------------------
+  ! !USES:
+  use ESMF_TestMod     ! test methods
+  use ESMF
+  use ESMF_AttributesMod
+
+  implicit none
+
+  !----------------------------------------------------------------------------
+  ! The following line turns the CVS identifier string into a printable variable.
+  character(*), parameter :: version = '$Id$'
+  !----------------------------------------------------------------------------
+
+  ! individual test failure message
+  character(ESMF_MAXSTR) :: failMsg
+  character(ESMF_MAXSTR) :: name
+
+  character(ESMF_MAXSTR) :: key = "foo"
+  integer               :: rc, petCount, ii
+  type(ESMF_VM)         :: vm
+  ! cumulative result: count failures; no failures equals "all pass"
+  integer               :: result = 0
+
+  integer(ESMF_KIND_I4) :: desired = 999, actual
+  type(ESMF_Attributes) :: attrs, attrs2
+  type(ESMF_Array)      :: array
+  type(ESMF_DistGrid)   :: distgrid
+  real(ESMF_KIND_R8)    :: farray2D(10,10)
+
+  logical :: failed
+
+  !----------------------------------------------------------------------------
+  call ESMF_TestStart(ESMF_SRCLINE, rc=rc)  ! calls ESMF_Initialize() internally
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  !----------------------------------------------------------------------------
+
+  call ESMF_VMGetGlobal(vm, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_VMGet(vm, petCount=petCount, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  !----------------------------------------------------------------------------
+  !NEX_UTest
+  write(name, *) "Get and manipulate Array attributes"
+  write(failMsg, *) "Did not set/get attribute"
+  rc = ESMF_FAILURE
+
+  distgrid = ESMF_DistGridCreate(minIndex=(/1,1/), maxIndex=(/10,10/), rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  array = ESMF_ArrayCreate(distgrid, farray2D, ESMF_INDEX_GLOBAL, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_ArrayGet(array, attrs=attrs, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_AttributesSet(attrs, key, desired, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_ArrayGet(array, attrs=attrs2, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_AttributesGet(attrs2, key, actual, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_ArrayDestroy(array, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  call ESMF_DistGridDestroy(distgrid, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_Test((actual == desired), name, failMsg, result, ESMF_SRCLINE)
+
+  !----------------------------------------------------------------------------
+  call ESMF_TestEnd(ESMF_SRCLINE) ! calls ESMF_Finalize() internally
+  !----------------------------------------------------------------------------
+
+end program ESMF_ArrayAttributesUTest
