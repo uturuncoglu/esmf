dfc656b2363b11a45ef1cdc950d70c2f0c10d7a8
Ben Koziol <ben.koziol@noaa.gov>
2019-07-26 16:06:40 -0600

    WIP: need to blow-up esmf and restart

--- src/Superstructure/Component/src/ESMCI_FTable.C
+++ src/Superstructure/Component/src/ESMCI_FTable.C
@@ -1852,9 +1852,14 @@ std::cout << "calling out of case FT_VOIDP1INTP" << "\n";
           localrc = comp->getBase(&base);
           if (ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU,
             ESMC_CONTEXT, &rc)) return rc; // bail out
-          ESMC_Logical presentFlag;
-          base->ESMC_BaseGetRoot()->AttributeIsPresent("ESMF_RUNTIME_COMPLIANCEICREGISTER",
-            &presentFlag);
+//          ESMC_Logical presentFlag;
+//          base->ESMC_BaseGetRoot()->AttributeIsPresent("ESMF_RUNTIME_COMPLIANCEICREGISTER",
+//            &presentFlag);
+          ESMCI::Info2 *info = base->ESMC_BaseGetInfo();
+          std::string key_cr = "ESMF_RUNTIME_COMPLIANCEICREGISTER";
+          bool presentFlag = info->hasKey(key_cr, localrc, false);
+          if (ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU,
+            ESMC_CONTEXT, &rc)) return rc; // bail out
 
 //#ifdef ESMF_NO_DLFCN
           //if (presentFlag==ESMF_TRUE){
@@ -1865,39 +1870,44 @@ std::cout << "calling out of case FT_VOIDP1INTP" << "\n";
 //#endif
 
 
-          if (presentFlag==ESMF_TRUE){
+//          if (presentFlag==ESMF_TRUE){
+          if (presentFlag){
 
-            // access the attribute object in base
-            ESMCI::Attribute *attr=base->ESMC_BaseGetRoot()->AttributeGet(
-              "ESMF_RUNTIME_COMPLIANCEICREGISTER");
+            // get the attribute value
+            //tdk:?: does this use a default convention and purpose?
+            std::string value = info->get<std::string>(key_cr, localrc);
 
-            // retrieve the string value of the attribute
-            std::vector<string> value;
-            localrc = attr->get(&value);
-            if (ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU,
-              ESMC_CONTEXT, &rc)) return rc; // bail out
-
-            // check the number of strings associated with the attribute
-            if (value.size()!=1){
-              ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_BAD,
-                "Attribute must contain single string", ESMC_CONTEXT, &rc);
-              return rc;
-            }
+//            // access the attribute object in base
+//            ESMCI::Attribute *attr=base->ESMC_BaseGetRoot()->AttributeGet(
+//              "ESMF_RUNTIME_COMPLIANCEICREGISTER");
+//
+//            // retrieve the string value of the attribute
+//            std::vector<string> value;
+//            localrc = attr->get(&value);
+//            if (ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU,
+//              ESMC_CONTEXT, &rc)) return rc; // bail out
+//
+//            // check the number of strings associated with the attribute
+//            if (value.size()!=1){
+//              ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_BAD,
+//                "Attribute must contain single string", ESMC_CONTEXT, &rc);
+//              return rc;
+//            }
 
             // convert routine name according to name mangeling mode
             //TODO: this would be a good Util method to have
 #ifdef ESMF_LOWERCASE_DOUBLEUNDERSCORE
-            std::transform(value[0].begin(), value[0].end(), value[0].begin(),
+            std::transform(value.begin(), value.end(), value.begin(),
               ::tolower);
-            value[0]+="__";
+            value+="__";
 #else
-            std::transform(value[0].begin(), value[0].end(), value[0].begin(),
+            std::transform(value.begin(), value.end(), value.begin(),
               ::tolower);
-            value[0]+="_";
+            value+="_";
 #endif
 
 #if 0
-std::cout << "ESMF_RUNTIME_COMPLIANCEICREGISTER attribute:" << value[0] <<"\n";
+std::cout << "ESMF_RUNTIME_COMPLIANCEICREGISTER attribute:" << value <<"\n";
 #endif
 
 #ifndef ESMF_NO_DLFCN
@@ -1929,17 +1939,17 @@ std::cout << "ESMF_RUNTIME_COMPLIANCEICREGISTER attribute:" << value[0] <<"\n";
             //routines have been linked in, which is typically the case
 
             //std::cout << "ESMF_RUNTIME_COMPLIANCEICREGISTER attribute:" << value[0] <<"\n";
-            if (value[0].find("nuopc_model") != std::string::npos) {
+            if (value.find("nuopc_model") != std::string::npos) {
                 FTN_X(nuopc_model_complianceicr)((void *)comp, &registerIcUserRc);
                 if (ESMC_LogDefault.MsgFoundError(registerIcUserRc,
                               ESMCI_ERR_PASSTHRU, ESMC_CONTEXT, &rc)) return rc; // bail out
             }
-            else if (value[0].find("nuopc_driver") != std::string::npos) {
+            else if (value.find("nuopc_driver") != std::string::npos) {
                 FTN_X(nuopc_driver_complianceicr)((void *)comp, &registerIcUserRc);
                 if (ESMC_LogDefault.MsgFoundError(registerIcUserRc,
                               ESMCI_ERR_PASSTHRU, ESMC_CONTEXT, &rc)) return rc; // bail out
             }
-            else if (value[0].find("nuopc_connector") != std::string::npos) {
+            else if (value.find("nuopc_connector") != std::string::npos) {
                 FTN_X(nuopc_connector_complianceicr)((void *)comp, &registerIcUserRc);
                 if (ESMC_LogDefault.MsgFoundError(registerIcUserRc,
                               ESMCI_ERR_PASSTHRU, ESMC_CONTEXT, &rc)) return rc; // bail out
